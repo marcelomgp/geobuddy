f6e6ad50a892009ca5f46f7b3a3375a0a7bd4402
menezes <marcelo.menezes@prodec.com.br>
2014-06-04 15:04:07 -0300

    new marker added

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,8 +1,35 @@
 $(document).ready(function() {
-  var map = L.map('map').setView([-22.896893,-43.282578], 13);
+  
+  // Map declare
+  var map = L.map('map').setView([-22.907675,-43.291499], 14);
 
   // add an OpenStreetMap tile layer
-  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
+  //L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  L.tileLayer('http://{s}.tile.cloudmade.com/4d8c9d1958ce4c1582ae158ce26bc516/997/256/{z}/{x}/{y}.png?{foo}',{foo: 'bar'}).addTo(map);
+
+  // Icons declare
+  var buddyIcon = L.icon({
+    iconUrl: 'http://cdn.leafletjs.com/leaflet-0.6.4/images/marker-icon.png',
+    //iconSize: [20, 30],
+    iconAnchor: [0, 0],
+    popupAnchor: [100, 100]
+  });
+
+  var iconOffsetted = L.icon({
+    iconUrl: 'http://cdn.leafletjs.com/leaflet-0.6.4/images/marker-icon.png',
+    //iconSize: [20, 30],
+    iconAnchor: [100, 100],
+    popupAnchor: [100, 100]
+  });
+
+  /*var popup = L.popup([-22.907675,-43.291499])
+              .setContent('<p> Hello! </p>')
+              .openOn(map);*/
+
+  // add marker to map
+  L.marker([-22.907675,-43.291499], {icon: buddyIcon}).addTo(map);
+  L.marker([-22.907675,-43.291499], {icon: iconOffsetted}).addTo(map);
+
+  
 });
+
