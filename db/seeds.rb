106d9e17fec2adf1b0d9d72475b1fef5c1894bdd
menezes <marcelo.menezes@prodec.com.br>
2014-06-10 17:40:49 -0300

    seed file updated with reports - need to fix it

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,28 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-    OccurrenceType.create(
-                          [
-                            {name: 'Robbery'}, 
-                            {name: 'Accident'},
-                            {name: 'Suspect'},
-                            {name: 'Complaint'},
-                            {name: 'Missing Person'}
-                          ]
-                         )
+OccurrenceType.create([{name: 'Robbery'}, {name: 'Accident'}, {name: 'Suspect'}, {name: 'Complaint'}, {name: 'Missing Person'}])
+
+occurrence_description_seed = ['Occurrence_seed '+rand(1..100).to_s,
+                              'Occurrence_seed '+rand(1..100).to_s,
+                              'Occurrence_seed '+rand(1..100).to_s,
+                              'Occurrence_seed '+rand(1..100).to_s,
+                              'Occurrence_seed '+rand(1..100).to_s,
+                              'Occurrence_seed '+rand(1..100).to_s,
+                              'Occurrence_seed '+rand(1..100).to_s,
+                              'Occurrence_seed '+rand(1..100).to_s,
+                              'Occurrence_seed '+rand(1..100).to_s,
+                              'Occurrence_seed '+rand(1..100).to_s]
+                  
+#center = [-22.907675, -43.291499] # Méier, Rio de Janeiro
+#radius = 3 # miles
+#point_near = Geocoder::Calculations.random_point_near(center, radius)
+#point  = "POINT(#{point_near[0]} #{point_near[1]})"
+
+#params = {
+  #:occurrence_type  => OccurrenceType.all.sample,
+  #:description      => occurrence_description_seed.sample,
+  #:coordinate       => point
+#}
+
+#report = Occurrence.create(params)
