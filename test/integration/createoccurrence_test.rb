7cbdd4766ef01d176dd2b198148d26b0c545ab47
marcelomgp <marcelo.mgp@gmail.com>
2014-06-17 12:01:08 -0300

    integration tests

--- test/integration/createoccurrence_test.rb
+++ test/integration/createoccurrence_test.rb
@@ -3,21 +3,21 @@ require 'test_helper'
 class CreateoccurrenceTest < ActionDispatch::IntegrationTest
 
   test "Creating new occurrence with valid params" do
-    visit '/occurrences/new'
-    fill_in 'Description', with: 'Testing valid params'
+    visit '/occurrences/new?lat=-22.893413507746303&lng=-43.29265594482422'
+    #select_option 'Occurence type', with: 'Accident'
+    fill_in 'Description:', with: 'Testing valid params'
     click_button 'Create'
-    expect(page).to have_content 'Success'
-    response.should render_template("home") 
+  #  expect(page).to have_content 'Success'
+    assert_equal current_path, '/' 
   end
 
-  #scenario "Creating new occurrence with invalid params - no Description" do
-  #  visit '/'
-  #  click_map
-  #  click_link 'New Occurrence'
-  #  select_option 'Occurence type', with: => 'Accident'
-  #  click_button 'Create'
-  #  expect(page).to have_content 'Error'
-  #end
+  test "Creating new occurrence with invalid params - no Description" do
+    visit '/occurrences/new?lat=-22.89112046369391&lng=-43.271026611328125'
+    click_button 'Create'
+
+    assert_equal current_path, '/occurrences'
+    assert page.has_text?("Description can't be blank")
+  end
 
   #scenario "Creating new occurrence with invalid params - no Occurrence type" do
   #  visit '/'
