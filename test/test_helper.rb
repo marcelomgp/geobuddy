192a61f3d9049ec62f6f8ffb405eb681593ffaf1
marcelomgp <marcelo.mgp@gmail.com>
2014-06-19 16:02:21 -0300

    tests adjustment

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'capybara/rails'
+require 'minitest/pride'
 
 class ActionDispatch::IntegrationTest
   # Make the Capybara DSL available in all integration tests
